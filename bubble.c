67880ac4e812649764edd02ac29937c6e513a3ad
kajj8808 <71279997+kajj8808@users.noreply.github.com>
2023-11-27 20:29:27 +0900

    bubble 주석 추가 2023-11-27

--- bubble.c
+++ bubble.c
@@ -29,7 +29,8 @@ void bubbleSort(int list[], int n)
     for (i = n - 1; i > 0; i--)
     {
         for (j = 0; j < i; j++)
-        { // 앞뒤의 레코드를 비교한 후 교체
+        { 
+            // 인접한 앞뒤의 레코드를 비교한 후 교체
             if (list[j] > list[j + 1])
             {
                 swap(&list[j], &list[j + 1]);
@@ -43,10 +44,12 @@ void bubbleSort(int list[], int n)
 
 int main()
 {
+    // 20번 반복합니다.
     for (int i = 0; i < 20; i++)
     {
         srand(time(NULL));
         int ranNums[20];
+        // random값 20개를 생성합니다.
         for (int i = 0; i < 20; i++)
         {
             int ranNum = rand() % 100;
@@ -59,7 +62,8 @@ int main()
         printf("Bubble Sort\n");
         bubbleSort(ranNums, 20);
     }
-
+    // sort를 할때 움직인 평균 횟수를 출력합니다.
     printf("\nMove Count: %d\n", moveCount / 20);
+    // 20번 돌때 값이 이동한 이동 횟수를 출력합니다.
     printf("CompareCount Count: %d\n", compareCount / 20);
 }
