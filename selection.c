e4798b92086c183e067813238edec6443a4169e7
kajj8808 <71279997+kajj8808@users.noreply.github.com>
2023-11-27 20:15:04 +0900

    selection 코드 주석추가 2023-11-27

--- selection.c
+++ selection.c
@@ -30,13 +30,13 @@ void selectionSort(int list[], int n)
     for (i = 0; i < n - 1; i++)
     {
         least = i;
-        for (j = i + 1; j < n; j++) // 최솟값 탐색
-            if (list[j] < list[least])
+        for (j = i + 1; j < n; j++) // 가장 작은 값을 찾습니다.
+            if (list[j] < list[least]) 
             {
                 least = j;
                 compareCount++;
             }
-        swap(&list[i], &list[least]);
+        swap(&list[i], &list[least]); // 가장 작은 값과 위치를 swap합니다. 
         moveCount++;
     }
     printNums(list);
@@ -44,8 +44,10 @@ void selectionSort(int list[], int n)
 
 int main()
 {
+    // 20번 반복합니다.
     for (int i = 0; i < 20; i++)
     {
+        // random값 20개를 생성합니다.
         srand(time(NULL));
         int ranNums[20];
         for (int i = 0; i < 20; i++)
@@ -59,7 +61,9 @@ int main()
         printf("\n");
         printf("Selection Sort\n");
         selectionSort(ranNums, 20);
-        printf("\nMove Count: %d\n", moveCount / 20);
+        // sort를 할때 움직인 횟수를 출력합니다.
+        printf("\nMove Count: %d\n", moveCount / 20); 
+        // 20번 반복한 평균 이동 횟수를 출력합니다.
         printf("CompareCount Count: %d\n", compareCount / 20);
     }
 }
